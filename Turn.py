fce7c4348eb0b491402a0cd1a078839dc2132478
tkgka <52348220+tkgka@users.noreply.github.com>
2020-07-21 18:17:39 +0900

    .

--- Turn.py
+++ Turn.py
@@ -44,13 +44,33 @@ async def on_message(message):
                     k+=f"<@{out[i].id}> ({out[i]})\n"
                 await message.channel.send(f"{k}")       
         
+
+        if command.startswith("!turn"):
+                         
+                num = [e.strip() for e in command.split('!turn')][1]
+            
+                if(num == ""):
+                    num = 1
+                elif(num.isdigit() == False):
+                    num =1
+                elif(int(num) >= len(members)):
+                    num = len(members)
+                out = random.sample(members,int(num))
+                await message.channel.send("```winners\n```")
+                k=""
+                for i in range(0,len(out)):
+                    k+=f"<@{out[i].id}> ({out[i]})\n"
+                await message.channel.send(f"{k}")      
+
         if command.startswith("!help"):
             await message.channel.send("```'!돌려돌려돌림판' 으로 작동시킬수있습니다. \n 뒤에 붙인 숫자만큼 사람을 뽑습니다.```")
+            await message.channel.send("```you can use bot with '!turn' and Pick as many people as the number after '!turn'.```")
 
             
 
     else:
         await message.author.send("개인 메시지에서는 지원하지 않습니다.\n```'!돌려돌려돌림판' 으로 작동시킬수있습니다. \n 뒤에 붙인 숫자만큼 사람을 뽑습니다.```")
+        await message.author.send("you can only use this bot in the guild```")
     
 
 client.run(pw.token())
